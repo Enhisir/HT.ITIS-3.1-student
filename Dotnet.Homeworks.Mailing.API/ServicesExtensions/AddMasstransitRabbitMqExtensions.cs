429b79f1aef815317536d607276e9ee5423e7656
Matvey Sergeev <msergeev@crafttalk.ru>
2025-01-05 19:05:14 +0300

    added MassTransit

--- Dotnet.Homeworks.Mailing.API/ServicesExtensions/AddMasstransitRabbitMqExtensions.cs
+++ Dotnet.Homeworks.Mailing.API/ServicesExtensions/AddMasstransitRabbitMqExtensions.cs
@@ -1,12 +1,33 @@
 using Dotnet.Homeworks.Mailing.API.Configuration;
+using Dotnet.Homeworks.Mailing.API.Consumers;
+using Dotnet.Homeworks.Mailing.API.Helpers;
+using MassTransit;
+using Microsoft.Extensions.Options;
 
 namespace Dotnet.Homeworks.Mailing.API.ServicesExtensions;
 
 public static class AddMasstransitRabbitMqExtensions
 {
-    public static IServiceCollection AddMasstransitRabbitMq(this IServiceCollection services,
-        RabbitMqConfig rabbitConfiguration)
+    public static IServiceCollection AddMasstransitRabbitMq(this IServiceCollection services)
     {
-        throw new NotImplementedException();
+        var rabbitConfiguration = 
+            services
+                .BuildServiceProvider()
+                .GetService<IOptions<RabbitMqConfig>>()!
+                .Value;
+        return services.AddMassTransit(x =>
+        {
+            x.SetKebabCaseEndpointNameFormatter();
+            x.AddConsumers(AssemblyReference.Assembly);
+            x.UsingRabbitMq((context, cfg) => 
+            {
+                cfg.Host(rabbitConfiguration.Hostname, h =>
+                {
+                    h.Username(rabbitConfiguration.Username);
+                    h.Password(rabbitConfiguration.Password);
+                });
+                cfg.ConfigureEndpoints(context);
+            });
+        });
     }
 }
